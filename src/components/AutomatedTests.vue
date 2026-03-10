9523a2f89fab558381a4050772e80af57e598700
Facundo Crego <facundocrego@cloud.upwork.com>
2026-03-09 23:34:33 -0300

    Fix seach input padding

--- src/components/AutomatedTests.vue
+++ src/components/AutomatedTests.vue
@@ -85,7 +85,7 @@ async function reloadTests() {
         <font-awesome-icon icon="fa-solid fa-refresh" />
       </a>
     </div>
-    <input v-model="query" name="query" class="w-full mb-1 border-b-2 border-gray-200" label="query" placeholder="Type to search">
+    <input v-model="query" name="query" class="w-full mb-1 px-1 border-b-2 border-gray-200" label="query" placeholder="Type to search">
     <div class="flex items-center mb-2">
       <span class="text-[10px] text-gray-500 ml-auto">Showing {{ filteredPaths.length }} of {{ testsList.length }} total tests</span>
     </div>
