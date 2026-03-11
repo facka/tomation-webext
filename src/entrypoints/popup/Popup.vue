7ea617c9f26bb4aa4c2cb56c297495ce9db471bc
Facundo Crego <facundocrego@cloud.upwork.com>
2026-03-11 11:07:37 -0300

    Fix popup and make it look like sidepanel

--- src/entrypoints/popup/Popup.vue
+++ src/entrypoints/popup/Popup.vue
@@ -1,15 +1,7 @@
 <script setup lang="ts">
-function openOptionsPage() {
-  browser.runtime.openOptionsPage()
-}
+import Sidepanel from '../sidepanel/Sidepanel.vue'
 </script>
 
 <template>
-  <main class="w-[300px] px-4 py-5 text-center text-gray-700">
-    <div>Popup</div>
-
-    <button class="btn mt-2" @click="openOptionsPage">
-      Open Options
-    </button>
-  </main>
+  <Sidepanel />
 </template>
